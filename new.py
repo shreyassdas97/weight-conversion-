73dcdaed502c6bd0b85332c0c0dd0bccea183507
shreyassdas97 <shreyasdas97@gmail.com>
2025-07-01 18:14:44 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,14 @@
+pound_to_kg=0.45
+kg_to_pound=2.20
+x=float(input("what is your weight? "))
+unit=input("is it in 'p'ounds or 'k'ilograms ").lower()
+if unit == 'p':
+    a= pound_to_kg * (x)
+    print(a)
+elif unit == 'k':
+    b=x *kg_to_pound
+    print(b )
+if x < 100 :
+    print("normal weight")
+else :
+    print("sanskar mota hai " )
